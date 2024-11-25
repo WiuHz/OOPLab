5cb9d46ec088f4a308fa911762716b42b122d85e
WiuHz <tranngocminh270905@gmail.com>
2024-11-25 23:15:30 +0700

    Update DigitalVideoDisc.java

--- Lab03/Aims Project/src/DigitalVideoDisc.java
+++ Lab03/Aims Project/src/DigitalVideoDisc.java
@@ -21,12 +21,16 @@ public class DigitalVideoDisc {
 	public DigitalVideoDisc(String title) {
 		super();
 		this.title = title;
+		nbDigitalVideoDisc++;
+		id = nbDigitalVideoDisc;
 	}
 	public DigitalVideoDisc(String title, String category, float cost) {
 		super();
 		this.category = category;
 		this.title = title;
 		this.cost = cost;
+		nbDigitalVideoDisc++;
+		id = nbDigitalVideoDisc;
 	}
 	public DigitalVideoDisc(String title, String category, String director, float cost) {
 		super();
@@ -34,6 +38,8 @@ public class DigitalVideoDisc {
 		this.category = category;
 		this.title = title;
 		this.cost = cost;
+		nbDigitalVideoDisc++;
+		id = nbDigitalVideoDisc;
 	}
 	public DigitalVideoDisc(String title, String category, String director, int length, float cost) {
 		super();
@@ -42,6 +48,8 @@ public class DigitalVideoDisc {
 		this.director = director;
 		this.length = length;
 		this.cost = cost;
+		nbDigitalVideoDisc++;
+		id = nbDigitalVideoDisc;
 	}
 	public String getTitle() {
 		return title;
